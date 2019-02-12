0818123e2b6146820e2d1a72317996e51aed4183
saturn09 <nurtas977@gmail.com>
2019-02-12 22:47:18 +0600

    Run pipe jobs synchronously

--- signer.go
+++ signer.go
@@ -1,15 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"sort"
+	"strings"
+)
 
 // сюда писать код
 
-type jobFunc func(in, out chan interface{})
+type jobGenerator func() string
 
-func ExecutePipeline(jobs...jobFunc) {
-	
+// sync jobs handling
+func ExecutePipeline(jobs ...jobGenerator) {
+	var (
+		calculatedHashes []string
+		result           string
+	)
+	for _, job := range jobs {
+		result = job()
+		log.Println(result)
+		calculatedHashes = append(calculatedHashes, result)
+	}
+	CombineResults(calculatedHashes)
+}
+
+func syncJob(input int) jobGenerator {
+	return func() string {
+		return MultiHash(SingleHash(input))
+	}
 }
 
 func SingleHash(data int) string {
-	DataSignerCrc32(fmt.Sprint(data))
+	var strData, md5, crc32, nestedHash string
+	strData = fmt.Sprint(data)
+	md5 = DataSignerMd5(strData)
+	crc32 = DataSignerCrc32(strData)
+	nestedHash = DataSignerCrc32(md5)
+	return crc32 + "~" + nestedHash
+}
+
+func MultiHash(data string) string {
+	var result []string
+	for i := 0; i < 6; i ++ {
+		result = append(result, DataSignerCrc32(fmt.Sprint(i)+data))
+	}
+	return strings.Join(result, "")
+}
+
+func CombineResults(results []string) string {
+	sort.Strings(results)
+	combinedResult := strings.Join(results, "_")
+	fmt.Println(combinedResult)
+	return combinedResult
+}
+
+func main() {
+	var jobs []jobGenerator
+	for i := 0; i < 10; i ++ {
+		jobs = append(jobs, syncJob(i))
+	}
+	ExecutePipeline(jobs...)
 }
