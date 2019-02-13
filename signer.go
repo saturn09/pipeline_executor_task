3afbd8b14d21f4d875e2e56cfb60758e9de4d36a
saturn09 <nurtas977@gmail.com>
2019-02-13 15:02:23 +0600

    Pipeline executor passes first test

--- signer.go
+++ signer.go
@@ -2,63 +2,81 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"sort"
-	"strings"
+	"sync"
 )
 
 // сюда писать код
 
-type jobGenerator func() string
+func ExecutePipeline(jobs ...job) {
+	in := make(chan interface{})
+	out := make(chan interface{})
 
-// sync jobs handling
-func ExecutePipeline(jobs ...jobGenerator) {
-	var (
-		calculatedHashes []string
-		result           string
-	)
-	for _, job := range jobs {
-		result = job()
-		log.Println(result)
-		calculatedHashes = append(calculatedHashes, result)
-	}
-	CombineResults(calculatedHashes)
-}
+	for i, currJob := range jobs {
+		if len(jobs) > i + 1{
+			go currJob(out, in)
+		} else {
+			go currJob(in, out)
+		}
 
-func syncJob(input int) jobGenerator {
-	return func() string {
-		return MultiHash(SingleHash(input))
 	}
 }
 
-func SingleHash(data int) string {
-	var strData, md5, crc32, nestedHash string
-	strData = fmt.Sprint(data)
-	md5 = DataSignerMd5(strData)
-	crc32 = DataSignerCrc32(strData)
-	nestedHash = DataSignerCrc32(md5)
-	return crc32 + "~" + nestedHash
+func SingleHash(in, out chan interface{}) {
+	var strData, md5, nestedHash string
+	strData = fmt.Sprint(<-out)
+	mu := &sync.Mutex{}
+	go func(mu *sync.Mutex) { // md5
+		mu.Lock()
+		out <- DataSignerMd5(strData) // Lock goroutine to avoid parallel access
+		mu.Unlock()
+	}(mu)
+
+	go func() { // crc(md5)
+		md5 = fmt.Sprint(<-out)
+		out <- DataSignerCrc32(md5)
+	}()
+
+	go func() { // crc + crc(md5)
+		nestedHash = fmt.Sprint(<-out)
+		out <- DataSignerCrc32(strData) + "~" + nestedHash
+	}()
 }
 
-func MultiHash(data string) string {
-	var result []string
+func MultiHash(in, out chan interface{}) {
+	//var result []string
+	data := fmt.Sprint(<-out)
+	wg := &sync.WaitGroup{}
 	for i := 0; i < 6; i ++ {
-		result = append(result, DataSignerCrc32(fmt.Sprint(i)+data))
+		wg.Add(1)
+		go func(wg *sync.WaitGroup) {
+			defer wg.Done()
+			out <- DataSignerCrc32(fmt.Sprint(i) + data)
+		}(wg)
 	}
-	return strings.Join(result, "")
+	wg.Wait()
+
+	//result = append(result, DataSignerCrc32(fmt.Sprint(i)+data))
+	//return strings.Join(result, "")
 }
 
-func CombineResults(results []string) string {
-	sort.Strings(results)
-	combinedResult := strings.Join(results, "_")
-	fmt.Println(combinedResult)
-	return combinedResult
+func CombineResults(in, out chan interface{}) {
+	//sort.Strings(results)
+	//combinedResult := strings.Join(results, "_")
+	fmt.Println("combinedResult")
+	// Write total result to chan
 }
 
 func main() {
-	var jobs []jobGenerator
-	for i := 0; i < 10; i ++ {
-		jobs = append(jobs, syncJob(i))
+	jobs := []job{
+		job(func(in, out chan interface{}) {
+			for i := 0; i < 10; i ++ {
+				out <- i
+			}
+		}),
+		job(SingleHash),
+		job(MultiHash),
+		job(CombineResults),
 	}
+
 	ExecutePipeline(jobs...)
 }
