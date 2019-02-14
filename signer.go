4227b92b2ce157e41f310577276f4428ce48bf75
saturn09 <nurtas977@gmail.com>
2019-02-14 13:39:16 +0600

    Swap input and output channels at executepipelines

--- signer.go
+++ signer.go
@@ -14,40 +14,42 @@ func ExecutePipeline(jobs ...job) {
 	actualInput := make(chan interface{})
 	actualOutput := make(chan interface{})
 
-	for i, currJob := range jobs {
-		if i == 1 {
-			go currJob(actualInput, actualOutput)
-		} else {
-			go currJob(actualOutput, actualInput)
-		}
+	for _, currJob := range jobs {
+		currJob(actualInput, actualOutput)
+
+		actualInput = actualOutput
 	}
 	time.Sleep(1 * time.Millisecond)
 }
 
 func SingleHash(in, out chan interface{}) {
 	log.Println("logging singleHash")
-	var strData, md5, nestedHash string
-	strData = fmt.Sprint(<-in)
+	var md5, nestedHash string
 	mu := &sync.Mutex{}
-	go func(mu *sync.Mutex) { // md5
-		mu.Lock()
-		out <- DataSignerMd5(strData) // Lock goroutine to avoid parallel access
-		mu.Unlock()
-	}(mu)
-
-	go func() { // crc(md5)
-		md5 = fmt.Sprint(<-out)
-		out <- DataSignerCrc32(md5)
-	}()
-
 	wg := &sync.WaitGroup{}
 
-	wg.Add(1)
-	go func(wg *sync.WaitGroup) { // crc + crc(md5)
-		defer wg.Done()
-		nestedHash = fmt.Sprint(<-out)
-		out <- DataSignerCrc32(strData) + "~" + nestedHash
-	}(wg)
+	for val := range in {
+		wg.Add(1)
+		go func(wg *sync.WaitGroup, mu *sync.Mutex) {
+			defer wg.Done()
+			go func(mu *sync.Mutex) { // md5
+				mu.Lock()
+				out <- DataSignerMd5(val.(string)) // Lock goroutine to avoid parallel access
+				mu.Unlock()
+			}(mu)
+			go func() { // crc(md5)
+				md5 = fmt.Sprint(<-out)
+				out <- DataSignerCrc32(md5)
+			}()
+
+			go func(wg *sync.WaitGroup) { // crc + crc(md5)
+				defer wg.Done()
+				nestedHash = fmt.Sprint(<-out)
+				out <- DataSignerCrc32(val.(string)) + "~" + nestedHash
+			}(wg)
+		}(wg, mu)
+
+	}
 	wg.Wait()
 }
 
